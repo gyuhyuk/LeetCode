059508b9905fabc7272a8bc5c7f49d24e688dfb4
KyungGyuHyuk <112837687+gyuhyuk@users.noreply.github.com>
2025-09-11 16:22:01 +0900

    Time: 488 ms (30.99%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -4,3 +4,10 @@ FROM Products p JOIN Orders o ON p.product_id = o.product_id
 WHERE o.order_date LIKE '2020-02%'
 GROUP BY p.product_name
 HAVING SUM(o.unit) >= 100
+
+/*
+    product_id로 JOIN
+    WHERE절에서 2020년 2월 주문 필터링
+    product_name으로 그룹핑
+    HAVING절에서 unit의 합이 100개 이상인 것 필터링
+*/
