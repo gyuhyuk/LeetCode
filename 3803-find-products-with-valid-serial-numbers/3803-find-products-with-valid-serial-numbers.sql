689171a5fcc922b2d8b2cd8307991ddc7134104f
KyungGyuHyuk <112837687+gyuhyuk@users.noreply.github.com>
2025-09-22 18:37:10 +0900

    Time: 373 ms (27.67%), Space: 0B (100%) - LeetHub

---
+++ 3803-find-products-with-valid-serial-numbers/3803-find-products-with-valid-serial-numbers.sql
@@ -0,0 +1,4 @@
+SELECT product_id, product_name, description
+FROM products
+WHERE description COLLATE Latin1_General_CS_AS LIKE '% SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9] %' OR description COLLATE Latin1_General_CS_AS LIKE '%SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]' OR description COLLATE Latin1_General_CS_AS LIKE 'SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9] %'
+ORDER BY product_id;
